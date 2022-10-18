f7d4e9cdb4b81353b52d0cc6536b61bd08547ddb
grahamwwilson <gwwilson@ku.edu>
2022-10-18 17:30:25 -0500

    target bigjay partition

---
+++ gp_launchb.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --job-name=gpigb              # Job name
+#SBATCH --partition=bigjay            # Partition Name (Required)
+#SBATCH --mail-type=ALL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=gwwilson@ku.edu   # Where to send mail	
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mem=4gb                     # Job memory request
+#SBATCH --time=0-12:00:00             # Time limit days-hrs:min:sec
+#SBATCH --output=gpigb_%j.log         # Standard output and error log
+
+VERSION=$1
+echo 'Version '$VERSION
+
+echo 'Running job as '
+echo 'User '$USER
+
+pwd
+hostname
+date
+
+echo 'PATH '
+echo $PATH
+ 
+echo 'LD_LIBRARY_PATH'
+echo $LD_LIBRARY_PATH
+
+module load fftw3
+module list
+
+echo 'Run Guinea-Pig++ script'
+
+BEAMS=$2
+echo 'Beams set to '${BEAMS}
+ 
+MYWDIR=${WORK}/GPRuns/RunGP3-${VERSION}_${BEAMS}
+echo 'Creating directory '${MYWDIR}
+mkdir ${MYWDIR}
+MYXDIR=$WORK/GPInstall3/bin
+MYCDIR=$WORK/GPConfigFiles
+MYCDIRG=${MYCDIR}/gpconfigs
+
+echo 'Script defines'
+echo 'MYWDIR:  '${MYWDIR}
+echo 'MYXDIR:  '${MYXDIR}
+echo 'MYCDIR:  '${MYCDIR}
+echo 'MYCDIRG: '${MYCDIRG}
+
+cd ${MYWDIR}
+
+echo 'Now in output directory '
+pwd
+echo 'Making symbolic links to control file and beam files'
+ln -s ${MYCDIRG}/acc-${VERSION}.dat acc.dat
+ln -s ${MYCDIR}/electron_${BEAMS}.ini electron.ini
+ln -s ${MYCDIR}/positron_${BEAMS}.ini positron.ini
+
+ls -lrt
+
+echo 'Start execution'
+date
+
+accel=SETA_250GeV
+parms=lumi
+outfile=GPResults.out
+
+echo "Accelerator " ${accel}
+echo "Parameters  " ${parms}
+echo "Results file " ${outfile}
+
+${MYXDIR}/guinea-Prod ${accel} ${parms} ${outfile}
+
+#Clean up a bit by compressing output files
+gzip *.out
+#Compress *.dat files starting with b,c,h,m, or p characters like pairs.dat
+gzip [bchmp]*.dat
+
+# Make a copy of the input file for posterity
+cp acc.dat acc-Run${VERSION}.dat
+ 
+date
+
+exit
