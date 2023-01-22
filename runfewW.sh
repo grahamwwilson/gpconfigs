e212dd34024cb3dd9530c7e368a3642bbf09ee83
grahamwwilson <gwwilson@ku.edu>
2023-01-21 20:23:51 -0600

    more WW jobs

--- runfewW.sh
+++ runfewW.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-sbatch gp_launchbpW.sh 60
-sbatch gp_launchbpW.sh 61
-sbatch gp_launchbpW.sh 62
+sbatch gp_launchbpW.sh 80
+sbatch gp_launchbpW.sh 81
+sbatch gp_launchbpW.sh 82
+
+sbatch gp_launchbpW.sh 83
+sbatch gp_launchbpW.sh 84
+sbatch gp_launchbpW.sh 85
+
+sbatch gp_launchbpW.sh 86
+sbatch gp_launchbpW.sh 87
+sbatch gp_launchbpW.sh 88
+
+sbatch gp_launchbpW.sh 89
 
 exit
