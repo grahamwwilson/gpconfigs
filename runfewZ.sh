e4234d66d240d5308e8bd3b1b8ad057fa7aa7a29
grahamwwilson <gwwilson@ku.edu>
2024-10-05 13:04:31 -0500

    Add test config with waist_y=0 for ILCZ

--- runfewZ.sh
+++ runfewZ.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-sbatch gp_launchbpZ.sh 70
-sbatch gp_launchbpZ.sh 71
-sbatch gp_launchbpZ.sh 72
-sbatch gp_launchbpZ.sh 73
-sbatch gp_launchbpZ.sh 74
-sbatch gp_launchbpZ.sh 75
-sbatch gp_launchbpZ.sh 76
-sbatch gp_launchbpZ.sh 77
-sbatch gp_launchbpZ.sh 78
-sbatch gp_launchbpZ.sh 79
+#sbatch gp_launchbpZ.sh 70
+#sbatch gp_launchbpZ.sh 71
+#sbatch gp_launchbpZ.sh 72
+#sbatch gp_launchbpZ.sh 73
+#sbatch gp_launchbpZ.sh 74
+#sbatch gp_launchbpZ.sh 75
+#sbatch gp_launchbpZ.sh 76
+#sbatch gp_launchbpZ.sh 77
+#sbatch gp_launchbpZ.sh 78
+#sbatch gp_launchbpZ.sh 79
+sbatch gp_launchbpZ.sh 80
 
 exit
