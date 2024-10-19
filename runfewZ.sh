2f9a96d44b75db737f49f6190304396682e55657
grahamwwilson <gwwilson@ku.edu>
2024-10-19 16:50:57 -0500

    Add files for 41, 67, 85 mrad runs

--- runfewZ.sh
+++ runfewZ.sh
@@ -13,10 +13,16 @@
 
 # Outer and inner high statistics
 
-sbatch gp_launchbpZ.sh 91
-sbatch gp_launchbpZC.sh 92
+#sbatch gp_launchbpZ.sh 91
+#sbatch gp_launchbpZC.sh 92
 
-sbatch gp_launchbpZ.sh 93
-sbatch gp_launchbpZC.sh 94
+#sbatch gp_launchbpZ.sh 93
+#sbatch gp_launchbpZC.sh 94
+
+# Now include scattering angle in Bhabha input files
+
+sbatch gp_launchZ-Bhabha.sh 95 41
+sbatch gp_launchZ-Bhabha.sh 96 67
+sbatch gp_launchZ-Bhabha.sh 97 85
 
 exit
