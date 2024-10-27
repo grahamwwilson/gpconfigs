1331028d49d21b35835200e467e8962e4a8de7b8
grahamwwilson <gwwilson@ku.edu>
2024-10-27 15:23:44 -0500

    Increase beta_y by 20%

--- runfewZ.sh
+++ runfewZ.sh
@@ -58,6 +58,7 @@
 #sbatch gp_launchZ-Bhabha.sh 122 TenAngles-Backward
 #sbatch gp_launchZ-Bhabha.sh 123 TenAngles-Backward
 
-sbatch gp_launchZ-Bhabha.sh 124 TenAngles
+#sbatch gp_launchZ-Bhabha.sh 124 TenAngles
+sbatch gp_launchZ-Bhabha.sh 125 TenAngles
 
 exit
