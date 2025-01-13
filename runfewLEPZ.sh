3a6a5857c814301647dbb732b3c83c1f166f3e77
graham <Graham.Wilson@cern.ch>
2025-01-13 12:57:27 -0600

    R9 tests

--- runfewLEPZ.sh
+++ runfewLEPZ.sh
@@ -2,7 +2,8 @@
 
 # Not sure why I started calling these bp.
 
-sbatch gp_launchbpLEPZC.sh 111C
+#sbatch gp_launchbpLEPZC.sh 111C
+sbatch gp_launchbpLEPZC-R9.sh 111R
 
 #sbatch gp_launchbpLEPZC.sh 114
 #sbatch gp_launchbpLEPZ.sh 113
